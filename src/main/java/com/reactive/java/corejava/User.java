e3e8cd7a775036f0f04ce844e86889bb39f6fad9
Vishal Kumar <64398452+vishal-kumar-solum@users.noreply.github.com>
2023-07-10 18:37:13 +0530

    First commit

---
+++ src/main/java/com/reactive/java/corejava/User.java
@@ -0,0 +1,18 @@
+package com.reactive.java.corejava;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+}
