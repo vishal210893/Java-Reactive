16da245cd6b15ac042ff692a2b7232d067f29743
Vishal Kumar <64398452+vishal-kumar-solum@users.noreply.github.com>
2023-07-24 12:41:26 +0530

    image gen code refactor

--- src/main/java/com/reactive/java/corejava/ImageGenerator.java
+++ src/main/java/com/reactive/java/corejava/ImageGenerator.java
@@ -1,6 +1,7 @@
 package com.reactive.java.corejava;
 
 import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.cli.*;
 import org.apache.commons.io.FileUtils;
 import org.bouncycastle.crypto.digests.Blake2sDigest;
@@ -11,15 +12,14 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+@Slf4j
 public class ImageGenerator {
-    public static int[][] createPatterns(int m, int n, int[] size, boolean border, int id) throws NoSuchAlgorithmException {
+    public static PatternResult createPatterns(int m, int n, int[] size, boolean border, int id) {
+        PatternResult patternResult = new PatternResult();
         int[][] patterns = new int[size[0]][size[1]];
         if (border) {
             fillRectangle(patterns, 0, 0, size[0] / 2, size[1] / 2, 255);
@@ -27,37 +27,60 @@ public class ImageGenerator {
         }
 
         int length = Math.min(size[0], size[1]);
-        int[] row_units = getUnits(length, m, size[0]);
-        int[] col_units = getUnits(length, n, size[1]);
+        int[] rowUnits = getUnits(length, m, size[0]);
+        int[] colUnits = getUnits(length, n, size[1]);
 
         ArrayList<Integer> code = new ArrayList<>();
         if ((m * n - 4) / 8 > 0) {
-            Blake2sDigest digest = new Blake2sDigest(m * n - 4);
+            int digestBitSize = ((m * n - 4) / 8) * 8;
+            Blake2sDigest digest = new Blake2sDigest(digestBitSize);
             if ((m * n - 4) % 8 != 0) {
                 byte[] idBytes = Integer.toString(id % (1 << 8)).getBytes(StandardCharsets.UTF_8);
                 digest.update(idBytes, 0, idBytes.length);
-                // TODO
+                byte[] values = new byte[digest.getDigestSize()];
+                digest.doFinal(values, 0);
+                String hexValues = Hex.toHexString(values);
 
+                final String hexValuesBinaryString = String.format("%" + digestBitSize + "s", new BigInteger(hexValues, 16).toString(2)).replace(' ', '0');
+                final List<Integer> hexValuesBinaryArray = new ArrayList<>(hexValuesBinaryString.codePoints()
+                        .mapToObj(c -> Integer.parseInt(String.valueOf((char) c)))
+                        .toList());
+
+                // Convert the toFillValue to a binary string representation andPad the binary string with leading zeros to the desired bit length
+                final String toFillBinaryString = String.format("%" + (m * n - 4) % 8 + "s", Integer.toBinaryString(id / (1 << 8))).replace(' ', '0');
+                List<Integer> toFillBinaryArray = toFillBinaryString.codePoints()
+                        .mapToObj(c -> Integer.parseInt(String.valueOf((char) c)))
+                        .toList();
+
+                for (int bIdx = 0; bIdx < toFillBinaryArray.size(); bIdx++) {
+                    int b = toFillBinaryArray.get(toFillBinaryArray.size() - 1 - bIdx);
+                    int insertIndex = hexValuesBinaryArray.size() - 1 - bIdx;
+                    hexValuesBinaryArray.add(insertIndex, b);
+                }
+                code.addAll(hexValuesBinaryArray);
+                // Setting Code value
+                patternResult.setCode(hexValuesBinaryArray.stream().map(s -> String.valueOf(s)).toList());
             } else {
                 byte[] idBytes = Integer.toString(id).getBytes(StandardCharsets.UTF_8);
                 digest.update(idBytes, 0, idBytes.length);
                 byte[] values = new byte[digest.getDigestSize()];
                 digest.doFinal(values, 0);
-                String hexValues = Hex.toHexString(values);
                 for (int i = 0; i < values.length; i++) {
                     for (int j = 0; j < 8; j++) {
                         code.add(i * 8 + j, ((values[i] >> (7 - j)) & 1));
                     }
                 }
+                // Setting Code value
+                patternResult.setCode(Hex.toHexString(values));
             }
-
         } else {
-            //TODO
-            int[] values = new int[(m * n - 4) % 8];
-            for (int i = 0; i < values.length; i++) {
-                values[i] = (id >> i) & 1;
-            }
-            //code = values;
+            final String toFillBinaryString = String.format("%" + (m * n - 4) % 8 + "s", Integer.toBinaryString(id % (1 << 8))).replace(' ', '0');
+            code = new ArrayList<>(toFillBinaryString.codePoints()
+                    .mapToObj(c -> Integer.parseInt(String.valueOf((char) c)))
+                    .toList());
+            // Setting Code value
+            patternResult.setCode(toFillBinaryString.toCharArray());
+
         }
         code.add(0, 0);
         code.add(n - 1, 1);
@@ -65,11 +88,11 @@ public class ImageGenerator {
         code.add(0);
 
         int[][] resultPatterns = new int[size[0]][size[1]];
-        for (int rIdx = 0; rIdx < row_units.length - 1; rIdx++) {
-            for (int cIdx = 0; cIdx < col_units.length - 1; cIdx++) {
+        for (int rIdx = 0; rIdx < rowUnits.length - 1; rIdx++) {
+            for (int cIdx = 0; cIdx < colUnits.length - 1; cIdx++) {
                 int value = code.get(n * rIdx + cIdx) * 255;
-                for (int i = row_units[rIdx]; i < row_units[rIdx + 1]; i++) {
-                    for (int j = col_units[cIdx]; j < col_units[cIdx + 1]; j++) {
+                for (int i = rowUnits[rIdx]; i < rowUnits[rIdx + 1]; i++) {
+                    for (int j = colUnits[cIdx]; j < colUnits[cIdx + 1]; j++) {
                         resultPatterns[i][j] = value;
                     }
                 }
@@ -77,15 +100,15 @@ public class ImageGenerator {
         }
 
         if (border) {
-            int[][] patternsToChange = resizeSubArray(resultPatterns, row_units[0], row_units[row_units.length - 1], col_units[0], col_units[col_units.length - 1], length);
+            int[][] patternsToChange = resizeSubArray(resultPatterns, rowUnits[0], rowUnits[rowUnits.length - 1], colUnits[0], colUnits[colUnits.length - 1], length);
             for (int i = 0; i < patternsToChange.length; i++) {
-                System.arraycopy(patternsToChange[i], 0, resultPatterns[row_units[0] + i], col_units[0], patternsToChange[i].length);
+                System.arraycopy(patternsToChange[i], 0, resultPatterns[rowUnits[0] + i], colUnits[0], patternsToChange[i].length);
             }
         } else {
-            resultPatterns = resizeSubArray(resultPatterns, row_units[0], row_units[row_units.length - 1], col_units[0], col_units[col_units.length - 1], length);
+            resultPatterns = resizeSubArray(resultPatterns, rowUnits[0], rowUnits[rowUnits.length - 1], colUnits[0], colUnits[colUnits.length - 1], length);
         }
-
-        return resultPatterns;
+        patternResult.setPatterns(resultPatterns);
+        return patternResult;
     }
 
     private static int[] getUnits(int length, int units, int size) {
@@ -114,48 +137,39 @@ public class ImageGenerator {
         return subArray;
     }
 
-    private static Map<String, Map<String, Object>> imageGenerator(int m, int n, int[] size, boolean border, List<String> idList) throws NoSuchAlgorithmException {
+    private static Map<String, Map<String, Object>> imageGenerator(int m, int n, int[] size, boolean border, List<String> idList) {
         Map<String, Map<String, Object>> imgList = new HashMap<>();
 
         for (String id : idList) {
-            int[][] patterns = createPatterns(m, n, size, border, Integer.parseInt(id));
-            String code = getCodeFromPatterns(patterns, m * n - 4);
+            PatternResult patternResult = createPatterns(m, n, size, border, Integer.parseInt(id));
             Map<String, Object> imgInfo = new HashMap<>();
-            imgInfo.put("code", code);
-            imgInfo.put("patterns", patterns);
+            imgInfo.put("code", patternResult.getCode());
+            imgInfo.put("patterns", patternResult.getPatterns());
             imgList.put(id, imgInfo);
         }
         return imgList;
     }
 
-    private static String getCodeFromPatterns(int[][] patterns, int numBits) {
-        // Calculate code from patterns (similar to Python code)
-        // ...
-
-        return "code"; // Return the calculated code
-    }
-
     private static void saveImg(Map<String, Map<String, Object>> imgList, String filePath) {
-        for (String key : imgList.keySet()) {
-            Map<String, Object> imgInfo = imgList.get(key);
-            int[][] patterns = (int[][]) imgInfo.get("patterns");
-
-            BufferedImage image = new BufferedImage(patterns[0].length, patterns.length, BufferedImage.TYPE_BYTE_GRAY);
-            for (int i = 0; i < patterns.length; i++) {
-                for (int j = 0; j < patterns[0].length; j++) {
-                    image.setRGB(j, i, patterns[i][j] == 255 ? 0xFFFFFF : 0);
-                }
-            }
-            File outputFile = new File(filePath + key + "_esl.jpg");
+        imgList.forEach((key, value) -> {
             try {
-                ImageIO.write(image, "jpg", outputFile);
-            } catch (IOException e) {
-                e.printStackTrace();
+                Map<String, Object> imgInfo = imgList.get(key);
+                int[][] patterns = (int[][]) imgInfo.get("patterns");
+
+                BufferedImage image = new BufferedImage(patterns[0].length, patterns.length, BufferedImage.TYPE_BYTE_GRAY);
+                for (int i = 0; i < patterns.length; i++) {
+                    for (int j = 0; j < patterns[0].length; j++) {
+                        image.setRGB(j, i, patterns[i][j] == 255 ? 0xFFFFFF : 0);
+                    }
+                }
+                ImageIO.write(image, "jpg", new File(filePath + key + "_esl.jpg"));
+            } catch (IOException ex) {
+                log.error(ex.getMessage(), ex);
             }
-        }
+        });
     }
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) {
         // Create and configure the command-line options
         Options options = new Options();
         options.addOption(Option.builder().longOpt("pattern_size").hasArgs().desc("Pattern size (m, n)").build());
@@ -175,11 +189,11 @@ public class ImageGenerator {
             String idPath = cmd.getOptionValue("id_path");
             String filePath = cmd.getOptionValue("file_path");
 
-            List<String> eslIdList = FileUtils.readLines(new File(idPath), "UTF-8");
+            List<String> eslIdList = FileUtils.readLines(new File(idPath), StandardCharsets.UTF_8);
             Map<String, Map<String, Object>> results = imageGenerator(patternSize[0], patternSize[1], bezelSize, border, eslIdList);
             saveImg(results, filePath);
             try (FileWriter file = new FileWriter(filePath + "esl_info.json")) {
-                file.write(new Gson().toJson(results));
+                file.write(new Gson().toJson(Collections.singletonList(results)));
             }
 
         } catch (ParseException | IOException e) {
---
+++ src/main/java/com/reactive/java/corejava/PatternResult.java
@@ -0,0 +1,13 @@
+package com.reactive.java.corejava;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PatternResult {
+
+    int[][] patterns;
+
+    Object code;
+}
